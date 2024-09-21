dd88ba2be341b011b83be55bcfd57918fba4e1f3
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-09-21 16:21:35 +0600

    Update install-pkg.sh

--- install-pkg.sh
+++ install-pkg.sh
@@ -8,12 +8,16 @@ packages=(
     "wget"
     "mpv"
     "openssh"
-    # Add other packages here
+    # Add other necessary packages here
 )
 
 # Install packages
 for pkg in "${packages[@]}"; do
-    pkg install "$pkg" -y
+    if pkg install "$pkg" -y; then
+        echo "$pkg installed successfully."
+    else
+        echo "Failed to install $pkg. Continuing with the next package..."
+    fi
 done
 
 # Change shell to ZSH
