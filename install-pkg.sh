e2a8515c1935731de652df7e79552c82057415b5
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-09-21 15:59:12 +0600

    Add files via upload

---
+++ install-pkg.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# List of necessary packages
+packages=(
+    "zsh"
+    "git"
+    "curl"
+    "wget"
+    # Add other packages here
+)
+
+# Install packages
+for pkg in "${packages[@]}"; do
+    pkg install "$pkg" -y
+done
+
+# Change shell to ZSH
+chsh -s zsh
