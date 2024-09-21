18210017f01c52d99de6dc295293e88f0cd915c7
nahid6970 <93816648+nahid6970@users.noreply.github.com>
2024-09-21 16:10:17 +0600

    Update os.sh

--- os.sh
+++ os.sh
@@ -7,19 +7,10 @@ pkg update && pkg upgrade -y
 echo "Installing necessary packages..."
 ./install-pkg.sh
 
-# Setup auto suggestion and syntax highlighting for ZSH
-echo "Setting up ZSH auto suggestion and syntax highlighting..."
-git clone https://github.com/zsh-users/zsh-autosuggestions ${HOME}/.zsh/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${HOME}/.zsh/zsh-syntax-highlighting
-
-# Setup zshrc
-echo "Setting up .zshrc..."
-cp zshrc ~/.zshrc
-
 # Setup Termux properties
 echo "Setting up Termux properties..."
 mkdir -p ~/.termux
 cp termux.properties ~/.termux/termux.properties
 termux-reload-settings
 
-echo "Setup complete. Please restart Termux."
+echo "Setup complete. Bash is set as the default shell. Please restart Termux."
