5ef8c93b1d687d175f1813c3948df3389eb007df
Nahid Ahmed <nahid6970@gmail.com>
2024-09-22 15:27:10 +0600

    xx

--- scripts/fzff.sh
+++ scripts/fzff.sh
@@ -1,17 +1,15 @@
 # Fuzzy Finder setup
 if command -v fzf >/dev/null 2>&1; then
     # Function to fuzzy find files, including hidden files
-    ff() {
+    fzff() {
         local file
-        file=$(find . -type f -name '.*' -o -type f | fzf)
+        file=$(find . -type f -name '.*' -o -type f | fzf --bind 'alt-c:execute-silent(echo -n {} | termux-clipboard-set)+abort')
+
         if [[ -n $file ]]; then
-            # Copy the file path to the clipboard
-            echo -n "$file" | termux-clipboard-set  # Use `pbcopy` on macOS or `xclip` on Linux
-            echo "Copied: $file"  # Optional: Notify that the path was copied
+            nano "$file"  # Open the file with nano
         fi
     }
+
     # Bind the function to a shortcut, for example, Ctrl+F
-    bind -x '"\C-f": ff'
-    # Bind Alt+C to copy the file path
-    bind -x '"\e[1;5;67": ff'  # Alt+C in some terminal emulators
+    bind -x '"\C-f": fzff'
 fi
