f45d7570f01ed65574a85063cc05ec1d2b9361aa
Nahid Ahmed <nahid6970@gmail.com>
2024-09-23 08:32:48 +0600

    xx

--- os.sh
+++ os.sh
@@ -110,8 +110,8 @@ remove_repo() {
 while true; do
     echo ""
     echo -e "${YELLOW}Select an option:${NC}"
-    echo -e "${BLUE}1. Initial Setup${NC} ( Packages, Storage, Password, Font)"
-    echo -e "${BLUE}2. Copy .bashrc and termux.properties${NC}"
+    echo -e "${BLUE}1. Initial Setup${NC} [Packages, Storage, Password, Font]"
+    echo -e "${BLUE}2. Copy Files${NC}  [.bashrc + termux.properties]"
     echo -e "${BLUE}3. Remove the repo folder (ms3)${NC}"
     echo -e "${BLUE}4. Exit${NC}"
     echo ""
