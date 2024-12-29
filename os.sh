c91a437395355622b015268b0aed00781e640684
nahid <nahid6970@gmail.com>
2024-12-29 12:36:37 +0600

    xx

--- os.sh
+++ os.sh
@@ -258,18 +258,18 @@ remote_access_goto_d1() {
     local psexec_path="C:/msBackups/PSTools/PsExec64.exe"
     local displayswitch_path="C:/msBackups/Display/DisplaySwitch.exe"
 
-    echo -e "Connecting to the remote server to execute the tasks..."
+    echo -e "Connecting to the remote server to kill processes and execute DisplaySwitch..."
 
-    # Run the AutoHotkey script commands on the remote machine over SSH
+    # Run the AHK commands sequentially on the Windows remote system
     sshpass -p "$remote_password" ssh "$remote_user@$remote_host" \
-        "cmd.exe /c 'taskkill /F /IM dnplayer.exe ; taskkill /F /IM python.exe ; '$psexec_path' -i 1 '$displayswitch_path' /internal'" 
+        "cmd.exe /c 'taskkill /F /IM dnplayer.exe && taskkill /F /IM python.exe && $psexec_path -i 1 $displayswitch_path /internal'" || {
+        echo -e "${RED}Failed to execute remote commands.${NC}"
+        return 1
+    }
 
-    if [ $? -eq 0 ]; then
-        echo -e "${GREEN}Tasks executed successfully on the remote server.${NC}"
-    else
-        echo -e "${RED}One or more tasks failed to execute on the remote server.${NC}"
-    fi
+    echo -e "${GREEN}Commands executed successfully on the remote server.${NC}"
 }
+
 
 
 
