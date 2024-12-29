2d00274d21da013fe4ae31a4eed8d555ea10246b
nahid <nahid6970@gmail.com>
2024-12-29 12:21:19 +0600

    xx

--- os.sh
+++ os.sh
@@ -262,7 +262,7 @@ remote_access_goto_d1() {
 
     # Run the AutoHotkey script commands on the remote machine over SSH
     sshpass -p "$remote_password" ssh "$remote_user@$remote_host" \
-        "cmd.exe /c 'taskkill /F /IM dnplayer.exe ; taskkill /F /IM python.exe ; '$psexec_path' -i 1 '$displayswitch_path' /internal || echo PsExec failed'" 
+        "cmd.exe /c 'taskkill /F /IM dnplayer.exe ; taskkill /F /IM python.exe ; '$psexec_path' -i 1 '$displayswitch_path' /internal'" 
 
     if [ $? -eq 0 ]; then
         echo -e "${GREEN}Tasks executed successfully on the remote server.${NC}"
