641bd8e19b5d1dbb31a1049f9d06bad25412d75c
nahid <nahid6970@gmail.com>
2024-12-31 11:17:15 +0600

    xx

--- os.sh
+++ os.sh
@@ -301,7 +301,6 @@ menu_items=(
     "11:Remove Folder [ms3]:            remove_repo                             :$RED"
     "12:Exit:                           exit_script                             :$RED"
 )
-
 # Display the menu and handle user input
 while true; do
     # Clear the screen before showing the updated menu
