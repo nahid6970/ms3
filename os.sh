d0e35e10b8b7a400ba7cd1e9b57f3b3d24868ce5
nahid <nahid6970@gmail.com>
2025-01-02 19:33:52 +0600

    xx

--- os.sh
+++ os.sh
@@ -306,6 +306,26 @@ about_device() {
     fastfetch
 }
 
+# ntfy_notify() {
+#     clear
+#     # Initialize counter
+#     not_found_count=0
+#     # Infinite loop to check continuously
+#     while true; do
+#         # Check if "ntfy" exists in the output
+#         if rclone ls g00: | grep -i ntfy; then
+#             # Play the music file using mpv if "ntfy" is found
+#             mpv /storage/emulated/0/song/wwe/ww.mp3
+#         else
+#             # Increment the counter and display the message
+#             not_found_count=$((not_found_count + 1))
+#             echo "No 'ntfy' found in the output. Count: $not_found_count"
+#         fi
+#         # Wait for 30 seconds before checking again
+#         sleep 30
+#     done
+# }
+
 ntfy_notify() {
     clear
     # Initialize counter
@@ -334,8 +354,6 @@ ntfy_notify() {
         sleep 30
     done
 }
-
-
 
 
 ntfy_remove() {
