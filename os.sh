89bce4e73cc05542c8f61396fa0cb60f123870b0
nahid <nahid6970@gmail.com>
2025-01-02 19:21:34 +0600

    xx

--- os.sh
+++ os.sh
@@ -310,26 +310,22 @@ ntfy_notify() {
     clear
     # Initialize counter
     not_found_count=0
-
     # Infinite loop to check continuously
     while true; do
         # Check if "ntfy" exists in the output
-        if rclone ls g00: | grep -iq "ntfy"; then
+        if rclone ls g00: | grep -i ntfy; then
             # Play the music file using mpv if "ntfy" is found
             mpv /storage/emulated/0/song/wwe/ww.mp3
         else
             # Increment the counter and display the message
             not_found_count=$((not_found_count + 1))
             echo "No 'ntfy' found in the output. Count: $not_found_count"
-            
-            # Trigger vibration using am broadcast
-            am broadcast -a android.intent.action.VIBRATE --ei duration 500
         fi
-
         # Wait for 30 seconds before checking again
         sleep 30
     done
 }
+
 
 ntfy_remove() {
     # remove te ntfy file
