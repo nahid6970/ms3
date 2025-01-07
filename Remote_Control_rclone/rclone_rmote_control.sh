035f9f355f23ea2952a4ed20907cade4bad9b69a
nahid <nahid6970@gmail.com>
2025-01-07 19:14:47 +0600

    xx

--- Remote_Control_rclone/rclone_rmote_control.sh
+++ Remote_Control_rclone/rclone_rmote_control.sh
@@ -13,8 +13,8 @@ fi
 # Generate a unique ID for this request (e.g., timestamp or random number)
 unique_id=$(date +%s)  # You can use a random number here if needed
 
-# Combine the unique ID with the command and make sure the whole command is enclosed in double quotes
-user_command="$unique_id: \"$1\""
+# Concatenate all arguments into a single command and enclose it in double quotes
+user_command="$unique_id: \"$(echo "$@" | sed 's/\"/\\\"/g')\""
 
 # Write the command to the remote file using rclone rcat
 echo "$user_command" | rclone rcat "$REMOTE_COMMAND_FILE"
