f330436b9ddcef977197b075bd81f379006cde5e
nahid <nahid6970@gmail.com>
2025-01-10 00:01:24 +0600

    xx

--- os.sh
+++ os.sh
@@ -450,7 +450,7 @@ menu_items=(
     "14:About:                          about_device                            :$BLUE"
     " c:Close:                          Close_script                            :$RED"
     " e:Exit:                           exit_script                             :$RED"
-    " u:Git Pull [ms3]:                 update_ms3_repo                         :$BLUE"
+    " u:Git_Pull:                 update_ms3_repo                         :$BLUE"
 )
 
 # Display the menu and handle user input
