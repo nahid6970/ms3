a2ea46f1b4149cafc7c03380cc6c75f629085a53
nahid <nahid6970@gmail.com>
2025-01-13 15:18:37 +0600

    xx

--- os.sh
+++ os.sh
@@ -452,23 +452,6 @@ welcome_remove() {
     touch .hushlogin
 }
 
-rclone_decrypt() {
-    # remove te ntfy file
-    clear
-    echo "Clone ms1 ...."
-    git_clone https://github.com/nahid6970/ms1.git
-    echo "Decreypt rclone conf ...."
-    pip install pycryptodomex
-    python ~/ms3/locker/locker.py --decrypt ~/ms1/asset/rclone/rclone.conf.enc
-
-    SOURCE_CONF_FILE="$HOME/ms1/asset/rclone/rclone.conf"
-    RCLONE_CONFIG_DIR="$HOME/.config/rclone"
-
-    echo -e "Copying rclone.conf"
-    cp "$SOURCE_CONF_FILE" "$RCLONE_CONFIG_DIR/"
-
-}
-
 
 
 
@@ -484,7 +467,6 @@ menu_items=(
     " 8:Remove Folder [ms3]:            remove_repo                             :$RED"
     " 9:About:                          about_device                            :$BLUE"
     "10:Welcome Page:                   welcome_remove                          :$RED"
-    "11:Rclone-Dycrypt:                 rclone_decrypt                          :$RED"
     " c:Close:                          Close_script                            :$RED"
     " e:Exit:                           exit_script                             :$RED"
     " u:Git Pull [ms3]:                 update_ms3_repo                         :$BLUE"
