761f3ac54cca447dab7a0f62a9262658c87a8be8
nahid <nahid6970@gmail.com>
2025-01-13 17:30:35 +0600

    xx

--- os.sh
+++ os.sh
@@ -495,7 +495,6 @@ menu_items=(
 # Display the menu and handle user input
 while true; do
     # Clear the screen to refresh the menu
-    clear
     
     echo ""
     echo -e "${YELLOW}Select an option:${NC}"
