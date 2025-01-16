cb0942e37f7848654dfdfb1217485e1c2ed371ed
nahid <nahid6970@gmail.com>
2025-01-16 21:46:01 +0600

    xx

--- Remote_Control_rclone/rclone_rmote_control.sh
+++ Remote_Control_rclone/rclone_rmote_control.sh
@@ -10,14 +10,11 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-# Replace "cc" with "&&" as a whole word
-parsed_command=$(echo "$*" | sed 's/\bcc\b/&&/g')
-
 # Generate a unique ID for this request (e.g., timestamp or random number)
 unique_id=$(date +%s)  # You can use a random number here if needed
 
-# Combine the unique ID with the parsed command
-user_command="$unique_id: $parsed_command"
+# Combine the unique ID with the command
+user_command="$unique_id: $*"
 
 # Write the command to the remote file using rclone rcat
 echo "$user_command" | rclone rcat "$REMOTE_COMMAND_FILE"
