084ab810c87bfe23cb35198bc37ce685e9265498
nahid <nahid6970@gmail.com>
2025-01-27 00:31:39 +0600

    January 27, 2025, 12:31 AM

--- switch_termux_properties.sh
+++ switch_termux_properties.sh
@@ -4,6 +4,6 @@ if [ "$1" == "nvim" ]; then
     cp ~/ms3/termux_nvim.properties ~/.termux/termux.properties
     termux-reload-settings
 else
-    cp ~/ms3/termux_default.properties ~/.termux/termux.properties
+    cp ~/ms3/termux.properties ~/.termux/termux.properties
     termux-reload-settings
 fi
