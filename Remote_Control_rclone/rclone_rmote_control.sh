5f10007aa81985e6d5db22f598879aad0f6dfd9b
nahid <nahid6970@gmail.com>
2025-01-26 22:02:41 +0600

    January 26, 2025, 10:02 PM

--- Remote_Control_rclone/rclone_rmote_control.sh
+++ Remote_Control_rclone/rclone_rmote_control.sh
@@ -9,6 +9,11 @@ export HISTSIZE=1000
 export HISTCONTROL=ignoredups
 shopt -s histappend  # Append to history file instead of overwriting it
 
+# Enable reverse search with Up/Down keys for partially typed commands
+# This will use arrow keys to search through history based on the typed prefix
+bind '"\e[A": history-search-backward'
+bind '"\e[B": history-search-forward'
+
 # Function to send a command and wait for output
 function send_command() {
     local user_command=$1
